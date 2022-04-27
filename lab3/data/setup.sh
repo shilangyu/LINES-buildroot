36b1ef13b3e373ae3dc124240e4e7b0f9e1d66c5
shilangyu <xmarcinmarcin@gmail.com>
2022-04-27 23:27:44 +0200

    Add lab3 configs

--- lab3/data/setup.sh
+++ lab3/data/setup.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
+set -e
+
 # get buildroot
 wget https://buildroot.org/downloads/buildroot-2022.02.tar.xz
 tar -xJf buildroot-2022.02.tar.xz
-cd buildroot-2022.02
 
-# copy config
-cp ../.config .
+cp -r buildroot-2022.02 buildroot-2022.02-user
+
+# copy configs
+cp .config.admin buildroot-2022.02/.config
+cp .config.user buildroot-2022.02-user/.config
 
 # change vfat partition size
-sed -i.bak 's/32M/256M/g' board/raspberrypi4-64/genimage-raspberrypi4-64.cfg
+sed -i.bak 's/32M/256M/g' buildroot-2022.02/board/raspberrypi4-64/genimage-raspberrypi4-64.cfg
+sed -i.bak 's/32M/256M/g' buildroot-2022.02-user/board/raspberrypi4-64/genimage-raspberrypi4-64.cfg
 
-# create build
+# create images
+cd buildroot-2022.02
+make
+cd ../buildroot-2022.02-user
 make
